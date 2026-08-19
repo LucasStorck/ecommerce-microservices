ca97ce86464036e4a8357c4ef124b662058017d5
LucasStorck <lucasstorck@icloud.com>
2026-08-19 14:02:42 -0300

    (feat): implemented the SecurityConfig classes in api-gateway and discovery-service modules; (refactor): changed the application.properties to application.yaml in discovery-service module

---
+++ api-gateway/src/main/java/org/lucas/apigateway/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package org.lucas.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(ServerHttpSecurity serverHttpSecurity){
+    serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
+      .authorizeExchange(exchange ->
+        exchange.pathMatchers("/eureka/**")
+          .permitAll()
+          .anyExchange()
+          .authenticated())
+      .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()));
+    return serverHttpSecurity.build();
+  }
+}

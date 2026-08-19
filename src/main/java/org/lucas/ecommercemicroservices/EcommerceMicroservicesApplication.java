45814f46963169755a92c276544d18fd248cf200
LucasStorck <lucasstorck@icloud.com>
2026-08-19 13:29:33 -0300

    (build): setup multi-module architecture and docker build config

---
+++ src/main/java/org/lucas/ecommercemicroservices/EcommerceMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package org.lucas.ecommercemicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lucas/ecommercemicroservices/EcommerceMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lucas.ecommercemicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceMicroservicesApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

03df15eedd6f15d4d9b8872a5f1985298186f0ac
LucasStorck <lucasstorck@icloud.com>
2026-08-19 14:06:04 -0300

    (refactor): changed from SecurityFilterChain to SecurityWebFilterChain in the api-gateway SecurityConfig

--- api-gateway/src/main/java/org/lucas/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/org/lucas/apigateway/config/SecurityConfig.java
@@ -3,16 +3,16 @@ package org.lucas.apigateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
   @Bean
-  public SecurityFilterChain securityFilterChain(ServerHttpSecurity serverHttpSecurity){
+  public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity serverHttpSecurity){
     serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
       .authorizeExchange(exchange ->
         exchange.pathMatchers("/eureka/**")
